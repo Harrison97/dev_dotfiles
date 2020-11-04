8cbbf4190fc2fa76db0aee7f0aefee98cf2b84f0
Harrison Hayes <harrisonhayes97@yahoo.com>
2020-11-04 04:40:30 +0000

    Auto PlugInstall

--- user-config.sh
+++ user-config.sh
@@ -3,5 +3,5 @@ su -c "git clone https://github.com/Harrison97/dev_dotfiles.git ~/dev_dotfiles"
 cp ~/dev_dotfiles/.* ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+vim +Pluginstall --sync' +qa
 source ~/.zshrc
