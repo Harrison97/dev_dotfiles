0a011ca81b2a80defd26c4e489886e65c8702cf5
Harrison Hayes <HarrisonHayes97@yahoo.com>
2020-11-04 04:59:32 +0000

    edit

--- user-config.sh
+++ user-config.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-su -c "git clone https://github.com/Harrison97/dev_dotfiles.git ~/dev_dotfiles"
+git clone https://github.com/Harrison97/dev_dotfiles.git ~/dev_dotfiles
 cp ~/dev_dotfiles/.* ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
